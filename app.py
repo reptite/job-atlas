6004646ded56921998344e4663e19e38178a8f4b
gp-atlassian <gpidcock@atlassian.com>
2021-08-21 19:57:48 +1000

    add MVP where dashboard

--- app.py
+++ app.py
@@ -14,8 +14,47 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import umap
 import pickle
 
-# dev
-from vega_datasets import data
+wordnet_lemmatizer = WordNetLemmatizer()
+
+
+def text_input_clean(text):
+    tokens = [w for w in word_tokenize(text.lower()) if w.isalpha()]
+    noStops = [w for w in tokens if w not in stopwords.words('english')]
+    lemmatized = [wordnet_lemmatizer.lemmatize(w) for w in noStops]
+    lemmatizedStr = ' '.join(lemmatized)
+    return lemmatizedStr
+
+def find_user_on_map(user_embedding, plot_data, user_title, user_jd):
+    user_plot_data = pd.DataFrame(data=user_embedding, columns=['umap_x','umap_y'])
+    user_plot_data['job_title'] = user_title
+    user_plot_data['job_description'] = user_jd
+    user_plot_data['source'] = 'user'
+
+    jobs_scatter = alt.Chart(plot_data).mark_circle(size=60).encode(
+        alt.X('umap_x:Q'),
+        alt.Y('umap_y:Q'),
+        tooltip=['job_title'],
+        color='source'
+    )
+
+    user_scatter = alt.Chart(user_plot_data).mark_circle(size=60).encode(
+        alt.X('umap_x:Q'),
+        alt.Y('umap_y:Q'),
+        tooltip=['job_title'],
+        color='source',
+        # labels='job_title'
+    )
+
+    scatter = alt.layer(jobs_scatter,user_scatter)
+
+    chart = scatter.properties(
+        title='Job Map!',
+        width=600,
+        height=400
+    ).interactive()
+
+    return chart
+
 
 
 def main():
@@ -27,8 +66,12 @@ def main():
     if dashboard == 'Where are you now?':
 
         st.title('Find yourself on the job map')
+        # load data
+        job_embeddings = pd.read_csv('./static/job_embeddings.csv')
+        reducer = pickle.load(open('./static/reducer.sav', 'rb'))
+        tfidf_vectorizer = pickle.load(open('./static/tfidf_vectorizer.sav', 'rb'))
 
-        #dev add max characters
+        # Get user input
         user_title = st.text_input("Tell us your job title", value='Data Scientist')
         user_jd = st.text_input(
             "Give us a short description of what you do",
@@ -36,20 +79,15 @@ def main():
             max_chars=600
             )
         user_input = user_title + ' ' + user_jd
-        plot_data = pd.read_csv('./static/job_embeddings.csv')
-        reducer = pickle.load(open('./static/reducer.sav', 'rb'))
-        tfidf_vectorizer = pickle.load(open('./static/tfidf_vectorizer.sav', 'rb'))
+        user_input_clean = text_input_clean(user_input)
 
+        # vectorize and collapse
+        user_vector = tfidf_vectorizer.transform([user_input_clean])
+        user_embedding = reducer.transform(user_vector)
 
-
-        chart = alt.Chart(source).mark_circle(size=60).encode(
-            x='Horsepower',
-            y='Miles_per_Gallon',
-            color='Origin',
-            tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
-        ).interactive()
-
-        st.altair_chart(chart)
+        # get altair chart
+        atlas = find_user_on_map(user_embedding,job_embeddings, user_title, user_jd)
+        st.altair_chart(atlas)
     
     if dashboard == 'another-thing':
 
